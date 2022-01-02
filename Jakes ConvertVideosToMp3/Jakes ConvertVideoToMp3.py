49ead479ca105340baf0ae72ef4f15b51942ad4d
jakeobs-projects <85255674+jakeobs-projects@users.noreply.github.com>
2022-01-02 13:35:15 -0800

    Update Jakes ConvertVideoToMp3.py

--- Jakes ConvertVideosToMp3/Jakes ConvertVideoToMp3.py
+++ Jakes ConvertVideosToMp3/Jakes ConvertVideoToMp3.py
@@ -3,11 +3,7 @@ from moviepy.editor import *
 import os
 print('running')
 def main():
-                print()
-                print()
                 file_1 = input("Filename:> ")
-                print()
-                print()
                 videoClip = VideoFileClip(file_1+'.mp4')
                 audioclip = videoClip.audio
                 audioclip.write_audiofile(file_1+'.mp3')
@@ -16,5 +12,4 @@ def main():
                 file_to_remove = file_1+'.mp4'
                 os.remove(file_to_remove)
                 main()
-   
 main()
